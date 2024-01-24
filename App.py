33b4a6ecd1412c61dfa36b02c8f03acfe7199989
Md Saif <142157955+saifahmed8521@users.noreply.github.com>
2024-01-24 22:33:04 +0530

    Add files via upload

---
+++ App.py
@@ -0,0 +1,54 @@
+import matplotlib.pyplot as plt
+import streamlit as st
+import numpy as np
+import pandas as pd
+import pickle
+from streamlit_lottie import st_lottie
+# import base64
+
+
+st.title("Crypto Price Predictor")
+st.sidebar.header("")
+
+
+chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
+st.bar_chart(chart_data)
+
+chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
+st.line_chart(chart_data)
+
+chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
+st.area_chart(chart_data)
+
+
+
+
+# def main_page():
+#     st.markdown("# Main page ")
+#     st.sidebar.markdown("# Main page 🎈")
+
+# def page2():
+#     st.markdown("Bitcoin")
+#     st.sidebar.markdown("Bitcoin")
+
+# def page3():
+#     st.markdown("Etherium")
+#     st.sidebar.markdown("Etherium")
+
+
+# def page4():
+#     st.markdown("Neocoin")
+#     st.sidebar.markdown("Neocoin")
+
+
+# page_names_to_funcs = {
+#     "Home Page": main_page,
+#     "Bitcoin": page2,
+#     "Etherium": page3,
+#     "Neocoin" : page4
+# }
+
+# selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
+# page_names_to_funcs[selected_page]()
+
+   
---
+++ Bitcoin.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import pickle
+from streamlit_lottie import st_lottie
+
+st.title("Bitcoin")
+
+st.image('output5.png')
+
+if st.button('Predict'):
+    #   st.subheader(select_crypto)
+       st.image('output1.png')
+
+if st.button('Predict Next 30Days'):
+     #  st.subheader(select_crypto)
+       st.image('output3.png')
