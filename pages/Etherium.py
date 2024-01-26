b967092c7fe500068062f2259b7328b1255cebbc
Md Saif <142157955+saifahmed8521@users.noreply.github.com>
2024-01-27 00:30:33 +0530

    Etherium.py

--- pages/Etherium.py
+++ pages/Etherium.py
@@ -2,12 +2,13 @@ import streamlit as st
 
 st.title("Etherium")
 
+outputE3 = image.open('outputE1')
 st.image('outputE3.png')
        
-if st.button('Predict'):
-       #st.subheader(select_crypto)
-       st.image('outputE1.png')
+# if st.button('Predict'):
+#        #st.subheader(select_crypto)
+#        st.image('outputE1.png')
 
-if st.button('Predict Next 30Days'):
-     #  st.subheader(select_crypto)
-       st.image('outputE2.png')
+# if st.button('Predict Next 30Days'):
+#      #  st.subheader(select_crypto)
+#        st.image('outputE2.png')
