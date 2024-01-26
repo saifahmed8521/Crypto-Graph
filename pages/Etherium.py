a9fb8ceefe5750cd6ebaab191f71fad748660c2e
Md Saif <142157955+saifahmed8521@users.noreply.github.com>
2024-01-27 01:11:38 +0530

    Etherium.py

--- pages/Etherium.py
+++ pages/Etherium.py
@@ -8,8 +8,8 @@ st.image(outputE3)
        
 if st.button('Predict'):
        outputE1 = Image.open('outputE1.png')
-       st.image('outputE1.png')
+       st.image(outputE1)
 
 if st.button('Predict Next 30Days'):
        outputE2 = Image.open('outputE2.png')
-       st.image('outputE2.png')
+       st.image(outputE2)
