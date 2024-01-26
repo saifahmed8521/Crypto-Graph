81f015140eda1ba6f10001b30590786e24c5e7c0
Md Saif <142157955+saifahmed8521@users.noreply.github.com>
2024-01-27 02:41:56 +0530

    Etherium.py

--- pages/Etherium.py
+++ pages/Etherium.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from PIL import Image
+from streamlit_extras.dataframe_explorer import dataframe_explorer
 
 st.title("Etherium")
 
@@ -13,3 +14,8 @@ if st.button('Predict'):
 if st.button('Predict Next 30Days'):
        outputE2 = Image.open('outputE2.png')
        st.image(outputE2)
+
+
+Bitcoin = pd.read_csv('Etherium.csv')
+Bitcoin1 = dataframe_explorer(Bitcoin,case=False)
+st.dataframe(Bitcoin1, use_container_width=True)
