777f00a10bb94b1a68749a576d13cc9b37c40ab0
Md Saif <142157955+saifahmed8521@users.noreply.github.com>
2024-01-27 00:15:29 +0530

    Etherium.py

--- pages/Etherium.py
+++ pages/Etherium.py
@@ -1,10 +1,6 @@
-
 import streamlit as st
-import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import pickle
-from streamlit_lottie import st_lottie
 
 st.title("Etherium")
 
@@ -17,22 +13,3 @@ if st.button('Predict'):
 if st.button('Predict Next 30Days'):
      #  st.subheader(select_crypto)
        st.image('outputE2.png')
-
-
-# import json 
-# import streamlit as st 
-# from streamlit_lottie import st_lottie 
-
-# path = "etherium_ani.json"
-# with open(path,"r") as file: 
-# 	url = json.load(file) 
-
-# st_lottie(url, 
-# 	reverse=True, 
-# 	height=400, 
-# 	width=400, 
-# 	speed=1, 
-# 	loop=True, 
-# 	quality='high', 
-# 	key='Car'
-# )
