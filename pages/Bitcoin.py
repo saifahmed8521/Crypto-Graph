7600eedbfc7190355130007532d35523256833c6
Md Saif <142157955+saifahmed8521@users.noreply.github.com>
2024-01-27 01:24:04 +0530

    Bitcoin.py

--- pages/Bitcoin.py
+++ pages/Bitcoin.py
@@ -8,8 +8,8 @@ st.image(output5)
 
 if st.button('Predict'):
   output1 = Image.open('output1.png')
-  st.image('output1.png')
+  st.image(output1)
 
 if st.button('Predict Next 30Days'):
       output3 = Image.open('output3.png')
-      st.image('output3.png')
+      st.image(output3)
