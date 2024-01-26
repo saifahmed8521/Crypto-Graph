a9023bec4a937bde61c6bbf5f2e68581911d22a5
Md Saif <142157955+saifahmed8521@users.noreply.github.com>
2024-01-27 02:48:50 +0530

    Neocoin.py

--- pages/Neocoin.py
+++ pages/Neocoin.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from PIL import Image
 from streamlit_extras.dataframe_explorer import dataframe_explorer
+import pandas as pd
 
 st.title("Neocoin")
 
@@ -15,6 +16,6 @@ if st.button('Predict Next 30Days'):
      outputN1 = Image.open('outputN1.png')
      st.image(outputN1)
     
-Neocoin = pd.read_csv('Neocoin.csv')
+Neocoin = pd.read_csv('NEOCoin.csv')
 Neocoin1 = dataframe_explorer(Neocoin,case=False)
 st.dataframe(Neocoin1, use_container_width=True)
