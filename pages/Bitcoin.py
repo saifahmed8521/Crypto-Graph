d23b77196d7f4bcea5add3bb9e7fd269e7176581
Md Saif <142157955+saifahmed8521@users.noreply.github.com>
2024-01-27 00:20:24 +0530

    Bitcoin.py

--- pages/Bitcoin.py
+++ pages/Bitcoin.py
@@ -1,1 +1,13 @@
+import streamlit as st
 
+st.title("Bitcoin")
+
+# st.image('outputN2.png')
+
+# if st.button('Predict'):
+#      #  st.subheader(select_crypto)
+#        st.image('outputN.png')
+
+# if st.button('Predict Next 30Days'):
+#      #  st.subheader(select_crypto)
+#        st.image('outputN1.png')
